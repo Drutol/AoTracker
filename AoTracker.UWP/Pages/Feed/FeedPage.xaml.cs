65c7b547052332e3ca7d39180a6a8a4808e98024
Piotr Szymczak <dogier140@poczta.fm>
2019-09-10 21:16:59 +0200

    [UWP] Basic feed data flow

--- AoTracker.UWP/Pages/Feed/FeedPage.xaml.cs
+++ AoTracker.UWP/Pages/Feed/FeedPage.xaml.cs
@@ -15,8 +15,11 @@ using Windows.UI.Xaml.Navigation;
 using AoLibs.Navigation.UWP.Attributes;
 using AoLibs.Navigation.UWP.Pages;
 using AoTracker.Domain.Enums;
+using AoTracker.Infrastructure.Models;
 using AoTracker.Infrastructure.ViewModels.Feed;
+using AoTracker.UWP.Pages.Feed;
 using AoTracker.UWP.Utils;
+using GalaSoft.MvvmLight.Messaging;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,15 +28,40 @@ namespace AoTracker.UWP.Pages
     [NavigationPage(PageIndex.Feed)]
     public sealed partial class FeedPage : FeedPageBase
     {
+        private readonly Dictionary<FeedTabEntry, FeedTabPage> _childViewModels =
+            new Dictionary<FeedTabEntry, FeedTabPage>();
+
+        public class NewFeedTabViewModelMessage
+        {
+            public FeedTabEntry FeedTabEntry { get; set; }
+            public FeedTabPage FeedTabViewModel { get; set; }
+        }
+
         public FeedPage()
         {
+            Messenger.Default.Register<NewFeedTabViewModelMessage>(this, OnNewTabMessage);
             this.InitializeComponent();
+        }
+
+        private void OnNewTabMessage(NewFeedTabViewModelMessage message)
+        {
+            _childViewModels[message.FeedTabEntry] = message.FeedTabViewModel;
+
+            if(Pivot.SelectedItem == message.FeedTabEntry)
+                message.FeedTabViewModel.NavigatedTo();
         }
 
         public override void NavigatedTo()
         {
             base.NavigatedTo();
             ViewModel.NavigatedTo();
+        }
+
+        private void Pivot_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (FeedTabEntry)e.AddedItems[0];
+            if(_childViewModels.TryGetValue(item, out var vm))
+                vm.NavigatedTo();
         }
     }
 
---
+++ AoTracker.UWP/Pages/Feed/FeedTabPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using AoLibs.Navigation.UWP.Pages;
+using AoTracker.Infrastructure.Models;
+using AoTracker.Infrastructure.ViewModels.Feed;
+using GalaSoft.MvvmLight.Messaging;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AoTracker.UWP.Pages.Feed
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class FeedTabPage 
+    {
+        public static readonly DependencyProperty FeedTabEntryProperty = DependencyProperty.Register(
+            "FeedTabEntry", typeof(FeedTabEntry), typeof(FeedTabPage), new PropertyMetadata(default(FeedTabEntry), PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var page = (FeedTabPage)d;
+            if (e.NewValue != null)
+            {
+                Messenger.Default.Send(new FeedPage.NewFeedTabViewModelMessage
+                {
+                    FeedTabEntry = (FeedTabEntry)e.NewValue,
+                    FeedTabViewModel = page
+                });
+            }
+        }
+
+        public FeedTabEntry FeedTabEntry
+        {
+            get => (FeedTabEntry) GetValue(FeedTabEntryProperty);
+            set => SetValue(FeedTabEntryProperty, value);
+        }
+
+        public FeedTabPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public override void NavigatedTo()
+        {
+            ViewModel.TabEntry = FeedTabEntry;
+            base.NavigatedTo();
+            ViewModel.NavigatedTo();
+        }
+    }
+
+    public class FeedTabPageBase : PageBase<FeedTabViewModel>
+    {
+        
+    }
+}
